279bcef4cd604731eb92f33e34c04e812552cfe2
BhavyaGupta315 <bhavyagupta315@gmail.com>
2025-03-11 18:07:29 +0530

    Added DB Credentials

---
+++ models/Content.ts
@@ -0,0 +1,32 @@
+import mongoose,  {Schema, Types} from "mongoose";
+
+const contentType = ['image', 'article', 'video', 'audio'];
+
+const ContentSchema = new Schema({
+    link : {
+        type: String,
+        required: true
+    },
+    type : {
+        type : String,
+        enum : contentType,
+        required : true
+    },
+    title : {
+        type : String,
+        required : true
+    },
+    tags : [
+        {
+            type : Types.ObjectId,
+            ref : 'Tag'
+        }
+    ],
+    userId : {
+        type : Types.ObjectId,
+        ref : 'User',
+        required : true
+    }
+});
+
+export default mongoose.models.Content || mongoose.model('Content', ContentSchema);
