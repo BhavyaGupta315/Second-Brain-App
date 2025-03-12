59682a71a0661e96fb6d3426d06edd08d5ff8eb4
BhavyaGupta315 <bhavyagupta315@gmail.com>
2025-03-12 15:33:51 +0530

    Added Signin Backend

---
+++ app/api/v1/signin/route.ts
@@ -0,0 +1,64 @@
+import Users from "@/models/Users";
+import { NextRequest } from "next/server";
+import * as jwt from "jsonwebtoken";
+
+interface userSchema{
+    username : string,
+    password : string
+}
+
+
+export async function POST(req : NextRequest){
+    const body : userSchema = await req.json();
+    // console.log(body);
+    const username = body.username;
+    const password = body.password;
+    try{
+        const user = await Users.findOne({
+            username : username,
+            password : password
+        });
+        if(!user){
+            return new Response(JSON.stringify({message : "Wrong email password"}),{
+                status : 403,
+                headers : { "Content-Type": "application/json" }
+            });
+        }
+        const userId = user._id;
+
+        if (!process.env.JWT_SECRET) {
+            throw new Error("Missing JWT_SECRET in environment variables");
+        }
+        
+        const JWT_SECRET: string = process.env.JWT_SECRET;
+        const jwtToken = jwt.sign({userId : userId.toString()}, JWT_SECRET);
+        return new Response(JSON.stringify({token : jwtToken}),{
+            status : 200,
+            headers : { "Content-Type": "application/json" }
+        });
+
+    }catch(err){
+        return new Response(JSON.stringify({message : "Server error"}),{
+            status : 500,
+            headers : { "Content-Type": "application/json" }
+        });
+    }
+}
+
+// - Sign in    
+//     POST /api/v1/signin
+//     ```json
+//     {
+//        "username": "harkirat",
+//        "password": "123123"
+//     }
+//     ```
+//     **Returns** - 200
+//     ```json
+//     {
+//     	"token": "jwt_token"
+//     }
+//     ```
+    
+//     - 403 - Wrong email password
+//     - 500 - Internal server error
