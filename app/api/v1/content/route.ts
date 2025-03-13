5f3be81da570d23aa3ae851d6f255d1ff1b13552
BhavyaGupta315 <bhavyagupta315@gmail.com>
2025-03-13 16:27:07 +0530

    Added deleting Functionality

--- app/api/v1/content/route.ts
+++ app/api/v1/content/route.ts
@@ -9,7 +9,6 @@ interface ContentSchema{
     "type" : 'image'| 'article'| 'video' | 'audio',
     "title" : string,
     "tags" : string[],
-    "userId" : string
 }
 
 export async function POST(req : NextRequest){
@@ -33,14 +32,24 @@ export async function POST(req : NextRequest){
 
         // Ensure body.tags is now an array of ObjectIds
         body.tags  = tagIds;
-        console.log(body);
-        const content = await Content.create(body); 
+        const headers : (string | null)= req.headers.get("authorization");
+        if(headers == null){
+            return new Response(JSON.stringify({message : "Authorization header not present"}),{
+                status : 403,
+                headers : { "Content-Type": "application/json" }
+            });
+        }
+        const decoded = jwt.verify(headers, process.env.JWT_SECRET || "");
+        const userId = (decoded as {userId : string}).userId;
+        const content = await Content.create({
+            ...body,
+            userId : userId
+        }); 
         console.log(content);
         return new Response(JSON.stringify(content), {
             status: 200,
             headers: { "Content-Type": "application/json" }
         });
-
     }catch(err){
         return new Response(JSON.stringify({message : err}),{
             status : 500,
@@ -63,7 +72,7 @@ export async function GET(req : NextRequest){
     try{
         const content = await Content.find({
             userId : userId
-        });
+        }).populate("userId", "username");
         return new Response(JSON.stringify(content),{
             status : 200,
             headers : { "Content-Type": "application/json" }
@@ -74,4 +83,35 @@ export async function GET(req : NextRequest){
             headers : { "Content-Type": "application/json" }
         });
     }
+}
+
+export async function DELETE(req : NextRequest){
+    try{
+        const headers : (string | null)= req.headers.get("authorization");
+        const body : {id : string} = await req.json();
+        if(headers == null){
+            return new Response(JSON.stringify({message : "Authorization header not present"}),{
+                status : 403,
+                headers : { "Content-Type": "application/json" }
+            });
+        }
+        const decoded = jwt.verify(headers, process.env.JWT_SECRET || "");
+        const userId = (decoded as {userId : string}).userId;
+        dbConnect();
+        await Content.deleteOne({
+            _id : body.id,
+            userId : userId
+        });
+        return new Response(JSON.stringify({message : "Content deleted"}),{
+            status : 200,
+            headers : { "Content-Type": "application/json" }
+        });
+    }catch(err){
+        return new Response(JSON.stringify({message : err}),{
+            status : 500,
+            headers : { "Content-Type": "application/json" }
+        });
+    }
+     
+
 }
